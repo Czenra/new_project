c0da4638b2e055467128fbb1eafa64978d1f0fa3
Czenra <rybkaalex2015@mail.ru>
2022-12-08 16:49:14 +0300

    new things added

--- Main_Window.py
+++ Main_Window.py
@@ -8,6 +8,18 @@ class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('C:/Users/rybka/OneDrive/Рабочий стол/QtProject/main.ui', self)
+        self.pushButton.clicked.connect(self.run)
+
+    def run(self):
+        name = self.textEdit.text()
+        with open("data_about_users.txt", "w") as f:
+            f.write(f"{name}")
+        self.label_1.setText('OK')
+        self.pushButton.clicked.connect(self.test)
+
+    def test(self):
+        self.label_2.setText('OK')
+
 
 
 
